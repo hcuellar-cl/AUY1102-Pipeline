ee20387679d49da49f1e90f8217ac61be083a1b2
Hugo Cuellar <hcuellar.cl@gmail.com>
2025-11-15 12:00:02 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const apiKey = '12345-abcde-67890-fghij';
 const githubToken = 'ghp_1234567890abcdefghijklmnopqrstuvwx';
 
 function fetchData() {
-    fetch(`https://api.example.com/data?api_key=${apiKey}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error('Error:', error));
+  fetch(`https://api.example.com/data?api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
 }
 
 fetchData();
